0dd3a6d3e4654c88a505111f8e39c862ed89829e
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2021-08-08 19:01:04 +0200

    added annouce to userdata
    Signed-off-by: Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>

--- tool/infra-testing-tool/config/userdata.go
+++ tool/infra-testing-tool/config/userdata.go
@@ -3,8 +3,7 @@ package config
 import (
 	"bytes"
 	"encoding/base64"
-
-	"strconv"
+	"infratesting/iac/components/networking"
 	"text/template"
 )
 
@@ -22,42 +21,40 @@ func (c *Node) GenerateUserData() (s string, err error) {
 	case NodeTypePeer:
 		templ = peerUserData
 
-		values["port"] = strconv.Itoa(c.NodeAttributes.Port)
-		values["defaultGrpcPort"] = defaultGrpcPort
+		values["listener"] = c.NodeAttributes.Listener
+		values["announce"] = c.NodeAttributes.Announce
+		values["defaultGrpcPort"] = networking.BertyGRPCPort
 		values["rdvp"] = c.NodeAttributes.RDVPMaddr
 		values["relay"] = c.NodeAttributes.RelayMaddr
 		values["bootstrap"] = c.NodeAttributes.BootstrapMaddr
-		values["protocol"] = c.NodeAttributes.Protocol
 
 	case NodeTypeBootstrap:
 		templ = bootstrapUserData
 
-		values["port"] = strconv.Itoa(c.NodeAttributes.Port)
+		values["listener"] = c.NodeAttributes.Listener
 		values["rdvp"] = c.NodeAttributes.RDVPMaddr
 		values["relay"] = c.NodeAttributes.RelayMaddr
 		values["bootstrap"] = c.NodeAttributes.BootstrapMaddr
-		values["protocol"] = c.NodeAttributes.Protocol
 
 	case NodeTypeRDVP:
 		templ = rdvpUserData
 
-		values["port"] = strconv.Itoa(c.NodeAttributes.Port)
+		values["listener"] = c.NodeAttributes.Listener
 		values["peerId"] = c.NodeAttributes.PeerId
 		values["pk"] = c.NodeAttributes.Pk
-		values["protocol"] = c.NodeAttributes.Protocol
 
 	case NodeTypeRelay:
 		templ = relayUserData
 
-		values["port"] = strconv.Itoa(c.NodeAttributes.Port)
+		values["announce"] = c.NodeAttributes.Announce
+		values["listener"] = c.NodeAttributes.Listener
 		values["peerId"] = c.NodeAttributes.PeerId
 		values["pk"] = c.NodeAttributes.Pk
-		values["protocol"] = c.NodeAttributes.Protocol
 
 	case NodeTypeReplication:
 		templ = replicationUserData
 
-		values["defaultGrpcPort"] = defaultGrpcPort
+		values["defaultGrpcPort"] = networking.BertyGRPCPort
 		values["sk"] = base64.RawStdEncoding.EncodeToString(c.NodeAttributes.Sk)
 		values["secret"] = base64.RawStdEncoding.EncodeToString(c.NodeAttributes.Secret)
 
